c0ae7d5953dcd8dff11b854c99a7c25fa9e553b5
Michael Fellinger <m.fellinger@gmail.com>
2008-08-17 00:22:50 +0900

    Update start.rb

--- start.rb
+++ start.rb
@@ -1,10 +1,38 @@
 require 'ramaze'
+require 'maruku'
 require 'rake'
 
 class MainController < Ramaze::Controller
-  def index
-    sh 'rake', 'build'
-    respond File.read('ramaze.html')
+  helper :aspect
+
+  before_all do
+    sh 'rake', 'build:css'
+  end
+
+  def index(chapter = nil)
+    chapter ||= :head
+    @name = chapter.to_s.gsub(/\W+/, '-')
+    @file = __DIR__/"chapter/#@name.mkd"
+    @html = compile(@file).to_html_document
+  end
+
+  private
+
+  def compile(file)
+    content = [
+      'section/header.mkd', 'section/links.mkd', file
+    ].map{|f| File.read(f) }.join("\n\n")
+
+    # puts content
+    Maruku.new(content)
+  end
+
+  def section(name)
+    File.read("section/#{name}.mkd".downcase)
+  end
+
+  def use(name)
+    File.read("chapter/#{name}.mkd".downcase)
   end
 end
 
